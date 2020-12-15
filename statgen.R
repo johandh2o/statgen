75b1cebc01ce35194139e0b3747b30fb3bb23162
Johan <johandh2o@outlook.com>
2020-12-15 12:25:09 -0500

    New commit

--- statgen.R
+++ statgen.R
@@ -10,18 +10,18 @@
 
 library(genetics)
 library(tidyverse)
+library(qvalue)
+library(pegas)
 library(haplo.stats)
 
-library(qvalue)
-library(randomForest)
-
 ##################################################
-## Functions
+## Auxiliar functions
 ##################################################
 
 # define a convenience wrapper
 updated <- function(object, formula., ..., evaluate = TRUE){
-  update(object = object, formula. = formula., data = object$model, ..., evaluate = evaluate)
+  update(object = object, formula. = formula.,
+         data = object$model, ..., evaluate = evaluate)
 }
 
 ##################################################
@@ -32,6 +32,9 @@ updated <- function(object, formula., ..., evaluate = TRUE){
 fullData = read.delim("http://www.biostat.umn.edu/~cavanr/FMS_data.txt",
                      header=T, sep="\t")
 
+# Binary race variable: Caucasian, non-Caucasian
+fullData$Caucasian = ifelse(fullData$Race=='Caucasian', 1, 0)
+
 # Genotype data
 genotypeData = fullData[,4:226] %>% 
   dplyr::select (-c(b2b, pai1_4g5g, rs849409,
@@ -39,58 +42,33 @@ genotypeData = fullData[,4:226] %>%
                     carp.exon3_snp3, carp_exon5_snp1,
                     gdf8_1225t, gdf8_p198a, gdf8_e164k))
 
-
-# Check for HWE in one SNP
-HWE.exact(genotype(genotypeData$actn3_r577x, sep=''))
-
 # Traits and cofounders
 traits = fullData %>% 
-  dplyr::select(Gender, Age, Race, pre.BMI, NDRM.CH)
+  dplyr::select(Gender, Age, Race, Caucasian, pre.BMI, NDRM.CH)
 
 ##################################################
-## Association Analysis WITHOUT genotype imputation
+## Subpopulation detection
 ##################################################
 
-## Exercise 1: Genotypic association analysis, no covariates
-## SNPs: actn3_r577x and resistin_c180g
-## NDRM.CH is the change of strength in non-dominant arm after exercise
+matData = data.matrix(genotypeData)
+matData[is.na(genotypeData)] = 0
 
-# Significant association is found
-summary(aov(NDRM.CH ~ actn3_r577x, data=fullData))
-# No significant association is found
-summary(aov(NDRM.CH ~ resistin_c180g, data=fullData))
+PCA = prcomp(matData)
+data.frame(PC1 = PCA$"x"[,1],
+           PC2 = PCA$"x"[,2],
+           Race = traits$Race) %>%
+  ggplot(aes(x=PC1, y=PC2, color=Race)) +
+  geom_point() + theme_bw()
 
-
-
-## Exercise 2: Additive association analysis with covariates
-## One SNP
-## NDRM.CH is the change of strength in non-dominant arm after exercise
-
-# No significant association is found 
-summary(lm(NDRM.CH ~ allele.count(genotype(resistin_c180g, sep=""))[,1] 
-           + Age + Gender + pre.BMI, data=fullData))
-
-
-
-## Exercise 3: Complete factor association analysis with covariates
-## One SNP
-## NDRM.CH is the change of strength in non-dominant arm after exercise
-
-# Complete factor model
-m1 = lm(NDRM.CH ~ actn3_r577x + Race + Age + Gender + pre.BMI, data=fullData)
-m0 = updated(m1, .~.-actn3_r577x)
-anova(m1, m0)
-
-
-## Example 4:  Association analysis with covariates
-## All SNP
-## NDRM.CH is the change of strength in non-dominant arm after exercise
+##################################################
+## Genotypic association
+##################################################
 
 # Extract p value for every SNP
 k = ncol(genotypeData)
 pvalues = rep(NA, k)
 for(i in 1:k){
-  tryCatch( { m1 = lm(NDRM.CH ~ genotypeData[,i]+ Race + Age + Gender + pre.BMI, data=fullData) },
+  tryCatch( { m1 = lm(NDRM.CH ~ genotypeData[,i]+ Caucasian + Age + Gender + pre.BMI, data=fullData) },
             error = function(e) {m1 = lm(NDRM.CH ~ genotypeData[,i] + Age + Gender + pre.BMI, data=fullData)})
   m0 = updated(m1, .~.-genotypeData[,i])
   pvalues[i] =  (anova(m1,m0))$`Pr(>F)`[2]
@@ -99,48 +77,87 @@ for(i in 1:k){
 # Manhattan Plot
 plot(1:k, -log10(pvalues))
 abline(h = -log10(0.05))
-abline(h = -log10(1-(1-0.05)^(1/k)))
+abline(h = -log10(1-(1-0.05)^(1/k))) # Pvalue correction 
 
-# Which SNP are associated
+# Which SNP are associated using pvalue 0.05
 snpNaiv = which(pvalues <= 0.05, arr.ind=TRUE, useNames = FALSE)
 colnames(genotypeData)[snpNaiv]
 
+# Q values estimation
+qobj = qvalue(pvalues)
+plot(qobj)
+min(qobj$pi0.lambda)
+min(qobj$pi0.smooth)
+
+# False discovery rate estimation
+max(qobj$qvalues[qobj$pvalues <= 0.006])
+snpFDR = which(pvalues <= 0.006, arr.ind=TRUE, useNames = FALSE)
+colnames(genotypeData)[snpFDR]
 
 ##################################################
-## Association Analysis WITH unobservable phase
+## Haplotypic association 1
 ##################################################
 
-# Genotype for selected SNP
-geno = cbind.data.frame(
+# HW Equilibrium
+geno1 = genotypeData[,35:38]
+hw.test(df2genind(geno1, sep=''))
+
+# Haplotype regression
+geno1 = cbind.data.frame(
   substr(genotypeData$akt2_7254617,1,1), substr(genotypeData$akt2_7254617,2,2),
+  substr(genotypeData$akt2_rs892118,1,1), substr(genotypeData$akt2_rs892118,2,2), 
   substr(genotypeData$akt2_2304186,1,1), substr(genotypeData$akt2_2304186,2,2), 
-  substr(genotypeData$ampd1_c34t,1,1), substr(genotypeData$ampd1_c34t,2,2)
+  substr(genotypeData$akt2_969531,1,1), substr(genotypeData$akt2_969531,2,2)
 )
+geno1 = setupGeno(geno1)
 
-# Haplotype estimation
-HaploEM = haplo.em(geno, locus.label=c("akt2_7254617","akt2_2304186",'ampd1_c34t'), 
-                   control=haplo.em.control(min.posterior=1e-4))
-HaploEM
+haplomodel1a = haplo.glm(NDRM.CH~ geno1 * Caucasian + Age + Gender + pre.BMI, data=fullData,
+                       allele.lev=attributes(geno1)$unique.alleles, 
+                       control=haplo.glm.control(haplo.effect="additive"))
+aic1a = haplomodel1a$aic
+haplomodel1r = haplo.glm(NDRM.CH~ geno1 * Caucasian + Age + Gender + pre.BMI, data=fullData,
+                         allele.lev=attributes(geno1)$unique.alleles, 
+                         control=haplo.glm.control(haplo.effect="recessive"))
+aic1r = haplomodel1r$aic
+haplomodel1d = haplo.glm(NDRM.CH~ geno1 * Caucasian + Age + Gender + pre.BMI, data=fullData,
+                         allele.lev=attributes(geno1)$unique.alleles, 
+                         control=haplo.glm.control(haplo.effect="dominant"))
+aic1d = haplomodel1d$aic
 
-# Regression on haplotypes
-geno = setupGeno(geno)
+# The recessive model is better
+summary(haplomodel1r)
 
-haplomodel = haplo.glm(NDRM.CH~ geno + Age + Gender + pre.BMI, data=fullData,
-                       allele.lev=attributes(geno)$unique.alleles,
-                       control=haplo.glm.control(haplo.effect="recessive"))
-summary(haplomodel)
+##################################################
+## Haplotypic association 2
+##################################################
 
+# HW Equilibrium
+geno2 = genotypeData[,208:213]
+hw.test(df2genind(geno2, sep=''))
 
+# Haplotype regression
+geno2 = cbind.data.frame(
+  substr(genotypeData$visfatin_2041681,1,1), substr(genotypeData$visfatin_2041681,2,2),
+  substr(genotypeData$visfatin_6947766,1,1), substr(genotypeData$visfatin_6947766,2,2), 
+  substr(genotypeData$visfatin_3801272,1,1), substr(genotypeData$visfatin_3801272,2,2), 
+  substr(genotypeData$visfatin_10487820,1,1), substr(genotypeData$visfatin_10487820,2,2),
+  substr(genotypeData$visfatin_929604,1,1), substr(genotypeData$visfatin_929604,2,2),
+  substr(genotypeData$visfatin_10953502,1,1), substr(genotypeData$visfatin_10953502,2,2)
+)
+geno2 = setupGeno(geno2)
 
+haplomodel2a = haplo.glm(NDRM.CH~ geno2 + Caucasian + Age + Gender + pre.BMI, data=fullData,
+                         allele.lev=attributes(geno2)$unique.alleles, 
+                         control=haplo.glm.control(haplo.effect="additive"))
+aic2a = haplomodel2a$aic
+haplomodel2r = haplo.glm(NDRM.CH~ geno2 + Caucasian + Age + Gender + pre.BMI, data=fullData,
+                         allele.lev=attributes(geno2)$unique.alleles, 
+                         control=haplo.glm.control(haplo.effect="recessive"))
+aic2r = haplomodel2r$aic
+haplomodel2d = haplo.glm(NDRM.CH~ geno2 + Caucasian + Age + Gender + pre.BMI, data=fullData,
+                         allele.lev=attributes(geno2)$unique.alleles, 
+                         control=haplo.glm.control(haplo.effect="dominant"))
+aic2d = haplomodel2d$aic
 
-
-texreg(l = list(mod[[1]], mod[[139]], mod[[102]], mod[[210]]))
-
-
-# Other side analysis
-
-matData = data.matrix(genoData)
-matData[is.na(genoData)] <- 4
-
-PCA = prcomp(matData)
-plot(PCA$"x"[,1],PCA$"x"[,2],xlab="PC1",ylab="PC2")
+# The additive model is better
+summary(haplomodel2a)
